fca7190c5431d40e2aafe20f261ebd187844b710
marcodes <marcodesanctis2@gmail.com>
2020-05-31 12:49:55 +0100

    unused using

--- Orders.Backend/WeatherForecastService.cs
+++ Orders.Backend/WeatherForecastService.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Orders.Shared;
 using System;
 using System.Linq;
