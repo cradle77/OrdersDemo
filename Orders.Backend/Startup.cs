7c5f66c722003ff0a02f56eb2d579d4478605275
marcodes <marcodesanctis2@gmail.com>
2020-06-02 18:39:13 +0100

    bugfix

--- Orders.Backend/Startup.cs
+++ Orders.Backend/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Orders.Backend.Data;
-using Polly;
 using System;
 
 [assembly: FunctionsStartup(typeof(Orders.Backend.Startup))]
