ee9e7c0dba477beb7e8cf6d5543158ad376f46c8
cradle77 <marcodesanctis2@gmail.com>
2020-10-04 17:55:28 +0100

    start version

--- Orders.Backend/CartEntity.cs
+++ Orders.Backend/CartEntity.cs
@@ -8,46 +8,9 @@ using System.Threading.Tasks;
 
 namespace Orders.Backend
 {
-    public class CartEntity : ICartActions
+    public class CartEntity
     {
-        public Cart Cart { get; set; } = new Cart();
-
-        public DateTime TimeStamp { get; set; }
-
-        public void Add(Product product)
-        {
-            var item = this.Cart.Items.SingleOrDefault(p => p.Product.Id == product.Id);
-
-            if (item != null)
-            {
-                item.Quantity += 1;
-            }
-            else
-            {
-                this.Cart.Items.Add(new CartItem() 
-                {
-                    Product = product,
-                    Quantity = 1
-                });
-            }
-
-            this.TimeStamp = DateTime.Now;
-        }
-
-        public Task<Cart> Get()
-        {
-            return Task.FromResult(this.Cart);
-        }
-
-        public void SetOwner(string owner)
-        {
-            this.Cart.Owner = owner;
-        }
-
-        public void Delete()
-        {
-            Entity.Current.DeleteState();
-        }
+        
 
         [FunctionName(nameof(CartEntity))]
         public static Task Run([EntityTrigger] IDurableEntityContext ctx)
